ad50c1612dd74b49caa80272cb1820fc30885a91
nt <ntsoftware@gmail.com>
2025-10-19 16:10:52 +0200

    deploy kanaquiz 2.2.0

---
+++ precache-manifest.ac93c9ae3952934fb1f97a7a8b1ff2ca.js
@@ -0,0 +1,24 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "5191f7bdab881bb18bc6db1798b121e0.ttf"
+  },
+  {
+    "url": "86d0cc5ebcb8daa4e42edcfcb66c2b72.ttf"
+  },
+  {
+    "url": "89889688147bd7575d6327160d64e760.svg"
+  },
+  {
+    "url": "e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "url": "f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "revision": "f6fdc4ffa0a45aa447b939e01eedd62b",
+    "url": "index.html"
+  },
+  {
+    "url": "main.3e5db360aa645ef58f83.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,5 @@
+importScripts("precache-manifest.ac93c9ae3952934fb1f97a7a8b1ff2ca.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
